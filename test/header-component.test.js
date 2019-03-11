6218c16f8eef5227e9d64665a6ca4e7b73030144
miniponz <ponzini@gmail.com>
2019-03-11 13:32:42 -0700

    created user profile template

--- test/header-component.test.js
+++ test/header-component.test.js
@@ -3,6 +3,20 @@ const test = QUnit.test;
 
 QUnit.module('header test');
 
+function makeProfile(user) {
+    const html = /*html*/ `
+    <div id="profile">
+        <span>Name: ${user.displayName}</span>
+        <img id="profile-image" src="${user.photoUrl}" alt="user profile image">
+        <button>Sign Out</button>
+    </div>
+    `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
 test('function makes header template', assert => {
     //arrange
     const expected = /*html*/ `    
@@ -17,4 +31,24 @@ test('function makes header template', assert => {
     const result = makeHeader();
     //assert
     assert.htmlEqual(result, expected);
+});
+
+test('function makes profile template', assert => {
+    //arrange
+    const user = {
+        displayName: 'Carol',
+        photoUrl: './assets/banana-icon.jpg'
+    };
+
+    const expected = /*html*/ `
+        <div id="profile">
+            <span>Name: Carol</span>
+            <img id="profile-image" src="./assets/banana-icon.jpg" alt="user profile image">
+            <button>Sign Out</button>
+        </div>
+        `;
+    //act
+    const dom = makeProfile(user);
+    //assert
+    assert.htmlEqual(dom, expected);
 });
