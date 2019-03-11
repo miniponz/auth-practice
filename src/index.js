7d2419d9e36897ebc140bb75fca0fe651c396a17
miniponz <ponzini@gmail.com>
2019-03-11 16:59:50 -0700

    created fetch and returned pizza cat results from API server

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ loadHeader();
 
 loadGifs(images);
 
+const query = encodeURIComponent('pizza cat');
+const API_KEY = '04JYUtl012K7M66v68lBD84XX77aXd1g';
+const url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`;
+
+fetch(url)
+    .then(res => res.json())
+    .then(response => loadGifs(response))
