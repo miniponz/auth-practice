58372fc05d262d1244df54648f95a803eda7b3b6
miniponz <ponzini@gmail.com>
2019-03-11 11:54:00 -0700

    Configure firebase export auth

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyBJSQy4sKjodHFMu_wm7DUTDfslVDVBDbU",
+    authDomain: "bootcamp-due-project.firebaseapp.com",
+  };
+  
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
